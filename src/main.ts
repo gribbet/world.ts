e5e8f0fafd6ac445ee949156096207e6a79f6f6f
Graham <graham.gibbons@gmail.com>
2022-03-05 19:44:27 -0800

    Cleanup

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import { mat4 } from "gl-matrix";
 
-let cubeRotation = 0;
+let rotation = 0;
 
 const vertexShaderSource = `
 attribute vec4 position;
@@ -25,15 +25,38 @@ void main(void) {
 }
 `;
 
-main();
+const positions = [
+  -1.0, -1.0, 1.0, 1.0, -1.0, 1.0, 1.0, 1.0, 1.0, -1.0, 1.0, 1.0, -1.0, -1.0,
+  -1.0, -1.0, 1.0, -1.0, 1.0, 1.0, -1.0, 1.0, -1.0, -1.0, -1.0, 1.0, -1.0, -1.0,
+  1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, -1.0, -1.0, -1.0, -1.0, 1.0, -1.0, -1.0,
+  1.0, -1.0, 1.0, -1.0, -1.0, 1.0, 1.0, -1.0, -1.0, 1.0, 1.0, -1.0, 1.0, 1.0,
+  1.0, 1.0, -1.0, 1.0, -1.0, -1.0, -1.0, -1.0, -1.0, 1.0, -1.0, 1.0, 1.0, -1.0,
+  1.0, -1.0,
+];
 
-function main() {
-  const canvas = document.querySelector<HTMLCanvasElement>("#glcanvas");
+const indices = [
+  0, 1, 2, 0, 2, 3, 4, 5, 6, 4, 6, 7, 8, 9, 10, 8, 10, 11, 12, 13, 14, 12, 14,
+  15, 16, 17, 18, 16, 18, 19, 20, 21, 22, 20, 22, 23,
+];
+
+const faceColors = [
+  [1.0, 1.0, 1.0, 1.0],
+  [1.0, 0.0, 0.0, 1.0],
+  [0.0, 1.0, 0.0, 1.0],
+  [0.0, 0.0, 1.0, 1.0],
+  [1.0, 1.0, 0.0, 1.0],
+  [1.0, 0.0, 1.0, 1.0],
+];
+
+const vertexColors = faceColors.flatMap((_) => [..._, ..._, ..._, ..._]);
+
+start();
+
+function start() {
+  const canvas = document.querySelector<HTMLCanvasElement>("canvas");
   const gl: WebGLRenderingContext = canvas.getContext("webgl");
 
-  if (!gl) {
-    return;
-  }
+  if (!gl) return;
 
   function loadShader(type: number, source: string) {
     const shader = gl.createShader(type);
@@ -42,7 +65,7 @@ function main() {
     if (!gl.getShaderParameter(shader, gl.COMPILE_STATUS)) {
       console.log("Compilation failed", gl.getShaderInfoLog(shader));
       gl.deleteShader(shader);
-      return null;
+      return;
     }
 
     return shader;
@@ -68,55 +91,33 @@ function main() {
   const modelViewUniform = gl.getUniformLocation(program, "modelView");
 
   const positionBuffer = gl.createBuffer();
-
   gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
-
-  const positions = [
-    -1.0, -1.0, 1.0, 1.0, -1.0, 1.0, 1.0, 1.0, 1.0, -1.0, 1.0, 1.0, -1.0, -1.0,
-    -1.0, -1.0, 1.0, -1.0, 1.0, 1.0, -1.0, 1.0, -1.0, -1.0, -1.0, 1.0, -1.0,
-    -1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, -1.0, -1.0, -1.0, -1.0, 1.0, -1.0,
-    -1.0, 1.0, -1.0, 1.0, -1.0, -1.0, 1.0, 1.0, -1.0, -1.0, 1.0, 1.0, -1.0, 1.0,
-    1.0, 1.0, 1.0, -1.0, 1.0, -1.0, -1.0, -1.0, -1.0, -1.0, 1.0, -1.0, 1.0, 1.0,
-    -1.0, 1.0, -1.0,
-  ];
-
   gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(positions), gl.STATIC_DRAW);
 
-  const faceColors = [
-    [1.0, 1.0, 1.0, 1.0],
-    [1.0, 0.0, 0.0, 1.0],
-    [0.0, 1.0, 0.0, 1.0],
-    [0.0, 0.0, 1.0, 1.0],
-    [1.0, 1.0, 0.0, 1.0],
-    [1.0, 0.0, 1.0, 1.0],
-  ];
-
-  const colors = faceColors.flatMap((_) => [..._, ..._, ..._, ..._]);
-
-  const colorBuffer = gl.createBuffer();
-  gl.bindBuffer(gl.ARRAY_BUFFER, colorBuffer);
-  gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(colors), gl.STATIC_DRAW);
+  const vertexColorBuffer = gl.createBuffer();
+  gl.bindBuffer(gl.ARRAY_BUFFER, vertexColorBuffer);
+  gl.bufferData(
+    gl.ARRAY_BUFFER,
+    new Float32Array(vertexColors),
+    gl.STATIC_DRAW
+  );
 
   const indexBuffer = gl.createBuffer();
   gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, indexBuffer);
-
-  const indices = [
-    0, 1, 2, 0, 2, 3, 4, 5, 6, 4, 6, 7, 8, 9, 10, 8, 10, 11, 12, 13, 14, 12, 14,
-    15, 16, 17, 18, 16, 18, 19, 20, 21, 22, 20, 22, 23,
-  ];
-
   gl.bufferData(
     gl.ELEMENT_ARRAY_BUFFER,
     new Uint16Array(indices),
     gl.STATIC_DRAW
   );
 
-  var then = 0;
+  let last = performance.now();
 
-  function render(now: number) {
-    now *= 0.001;
-    const deltaTime = now - then;
-    then = now;
+  function render() {
+    const now = performance.now();
+    const deltaTime = (now - last) / 1000;
+    last = now;
+
+    rotation += deltaTime;
 
     gl.clearColor(0.0, 0.0, 0.0, 1.0);
     gl.clearDepth(1.0);
@@ -124,70 +125,38 @@ function main() {
     gl.depthFunc(gl.LEQUAL);
     gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
 
-    const fieldOfView = (45 * Math.PI) / 180;
-    const aspect = gl.canvas.clientWidth / gl.canvas.clientHeight;
-    const zNear = 0.1;
-    const zFar = 100.0;
+    const { clientWidth: width, clientHeight: height } = gl.canvas;
     const projection = mat4.create();
-
-    mat4.perspective(projection, fieldOfView, aspect, zNear, zFar);
+    mat4.perspective(
+      projection,
+      (45 * Math.PI) / 180,
+      width / height,
+      0.1,
+      100.0
+    );
 
     const modelView = mat4.create();
-
     mat4.translate(modelView, modelView, [-0.0, 0.0, -6.0]);
-    mat4.rotate(modelView, modelView, cubeRotation, [0, 0, 1]);
-    mat4.rotate(modelView, modelView, cubeRotation * 0.7, [0, 1, 0]);
+    mat4.rotate(modelView, modelView, rotation, [0, 0, 1]);
+    mat4.rotate(modelView, modelView, rotation * 0.7, [0, 1, 0]);
 
-    {
-      const numComponents = 3;
-      const type = gl.FLOAT;
-      const normalize = false;
-      const stride = 0;
-      const offset = 0;
-      gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
-      gl.vertexAttribPointer(
-        positionAttribute,
-        numComponents,
-        type,
-        normalize,
-        stride,
-        offset
-      );
-      gl.enableVertexAttribArray(positionAttribute);
-    }
+    gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
+    gl.vertexAttribPointer(positionAttribute, 3, gl.FLOAT, false, 0, 0);
+    gl.enableVertexAttribArray(positionAttribute);
 
-    {
-      const numComponents = 4;
-      const type = gl.FLOAT;
-      const normalize = false;
-      const stride = 0;
-      const offset = 0;
-      gl.bindBuffer(gl.ARRAY_BUFFER, colorBuffer);
-      gl.vertexAttribPointer(
-        vertexColorAttribute,
-        numComponents,
-        type,
-        normalize,
-        stride,
-        offset
-      );
-      gl.enableVertexAttribArray(vertexColorAttribute);
-    }
+    gl.bindBuffer(gl.ARRAY_BUFFER, vertexColorBuffer);
+    gl.vertexAttribPointer(vertexColorAttribute, 4, gl.FLOAT, false, 0, 0);
+    gl.enableVertexAttribArray(vertexColorAttribute);
 
     gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, indexBuffer);
     gl.useProgram(program);
     gl.uniformMatrix4fv(projectionUniform, false, projection);
     gl.uniformMatrix4fv(modelViewUniform, false, modelView);
 
-    {
-      const vertexCount = 36;
-      const type = gl.UNSIGNED_SHORT;
-      const offset = 0;
-      gl.drawElements(gl.TRIANGLES, vertexCount, type, offset);
-    }
+    gl.drawElements(gl.TRIANGLES, 36, gl.UNSIGNED_SHORT, 0);
 
-    cubeRotation += deltaTime;
     requestAnimationFrame(render);
   }
-  requestAnimationFrame(render);
+
+  render();
 }
