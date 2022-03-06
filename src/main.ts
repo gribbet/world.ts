c883278d4307cf8e937e0f636a7b6012cddf8b90
Graham <graham.gibbons@gmail.com>
2022-03-05 21:18:06 -0800

    Texture

--- src/main.ts
+++ src/main.ts
@@ -6,23 +6,31 @@ export function range(start: number, end: number) {
 
 const vertexShaderSource = `
 attribute vec4 position;
+attribute vec2 textureCoordinate;
 uniform mat4 modelView;
 uniform mat4 projection;
 
+varying highp vec2 textureCoordinateOut;
+
 void main(void) {
   gl_Position = projection * modelView * position;
+  textureCoordinateOut = textureCoordinate;
 }
 `;
 
 const fragmentShaderSource = `
+varying highp vec2 textureCoordinateOut;
+
+uniform sampler2D sampler;
+
 void main(void) {
-  gl_FragColor = vec4(1., 1., 1., 1.);
+  gl_FragColor = texture2D(sampler, textureCoordinateOut);
 }
 `;
 
-const n = 1;
+const n = 10;
 const positions = range(0, n + 1).flatMap((y) =>
-  range(0, n + 1).flatMap((x) => [x / n - 0.5, y / n - 0.5, 1])
+  range(0, n + 1).flatMap((x) => [(2 * x) / n - 1, 1 - (2 * y) / n, 1])
 );
 
 const indices = range(0, n).flatMap((y) =>
@@ -34,6 +42,10 @@ const indices = range(0, n).flatMap((y) =>
     (y + 1) * (n + 1) + x,
     (y + 1) * (n + 1) + x + 1,
   ])
+);
+
+const textureCoordinates = range(0, n + 1).flatMap((y) =>
+  range(0, n + 1).flatMap((x) => [x / n, y / n])
 );
 
 start();
@@ -72,8 +84,13 @@ function start() {
   }
 
   const positionAttribute = gl.getAttribLocation(program, "position");
+  const textureCoordinateAttribute = gl.getAttribLocation(
+    program,
+    "textureCoordinate"
+  );
   const projectionUniform = gl.getUniformLocation(program, "projection");
   const modelViewUniform = gl.getUniformLocation(program, "modelView");
+  const samplerUniform = gl.getUniformLocation(program, "sampler");
 
   const positionBuffer = gl.createBuffer();
   gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
@@ -86,6 +103,37 @@ function start() {
     new Uint16Array(indices),
     gl.STATIC_DRAW
   );
+
+  const textureCoordinateBuffer = gl.createBuffer();
+  gl.bindBuffer(gl.ARRAY_BUFFER, textureCoordinateBuffer);
+  gl.bufferData(
+    gl.ARRAY_BUFFER,
+    new Float32Array(textureCoordinates),
+    gl.STATIC_DRAW
+  );
+
+  const texture = gl.createTexture();
+  gl.bindTexture(gl.TEXTURE_2D, texture);
+  gl.texImage2D(
+    gl.TEXTURE_2D,
+    0,
+    gl.RGBA,
+    1,
+    1,
+    0,
+    gl.RGBA,
+    gl.UNSIGNED_BYTE,
+    new Uint8Array([0, 0, 0, 255])
+  );
+
+  const image = new Image();
+  image.crossOrigin = "anonymous";
+  image.onload = function () {
+    gl.bindTexture(gl.TEXTURE_2D, texture);
+    gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, image);
+    gl.generateMipmap(gl.TEXTURE_2D);
+  };
+  image.src = "http://mt0.google.com/vt/lyrs=y&hl=en&x=0&y=0&z=0";
 
   function render() {
     gl.clearColor(0, 0, 0, 1);
@@ -118,6 +166,22 @@ function start() {
     gl.enableVertexAttribArray(positionAttribute);
 
     gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, indexBuffer);
+
+    gl.bindBuffer(gl.ARRAY_BUFFER, textureCoordinateBuffer);
+    gl.vertexAttribPointer(
+      textureCoordinateAttribute,
+      2,
+      gl.FLOAT,
+      false,
+      0,
+      0
+    );
+    gl.enableVertexAttribArray(textureCoordinateAttribute);
+
+    gl.activeTexture(gl.TEXTURE0);
+    gl.bindTexture(gl.TEXTURE_2D, texture);
+    gl.uniform1i(samplerUniform, 0);
+
     gl.useProgram(program);
 
     gl.uniformMatrix4fv(projectionUniform, false, projection);
