0bff5c7a9c1be544f28885c60ad0d19357155923
Graham <graham.gibbons@gmail.com>
2022-05-11 21:24:50 -0700

    Remove unused mipmap

--- src/tiles.ts
+++ src/tiles.ts
@@ -19,7 +19,6 @@ export const createTiles: (gl: WebGLRenderingContext) => Tiles = (gl) => {
     gl,
     url: imageryUrl,
     onLoad: () => {
-      gl.generateMipmap(gl.TEXTURE_2D);
       gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
       gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.LINEAR);
       gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
