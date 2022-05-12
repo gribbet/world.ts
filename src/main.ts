f7c17245fc99728fa4f8653c69ae1c30794416a5
Graham <graham.gibbons@gmail.com>
2022-05-11 20:43:06 -0700

    Adjust TODO

--- src/main.ts
+++ src/main.ts
@@ -4,10 +4,8 @@ import { world } from "./world";
  * TODO:
  * explicit anchor
  * smooth transition
- * elevation tile -1
  * mercator elevation
  * subdivide const
- * pick inaccuracy
  */
 
 world(document.querySelector("canvas") as HTMLCanvasElement);
