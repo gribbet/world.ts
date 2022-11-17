cb5a44eba01e7e5697cce4ee7bd7d390f8c72172
Graham <graham.gibbons@gmail.com>
2022-11-16 23:47:33 -0800

    Mipmapping

--- src/layers/terrain/tiles.ts
+++ src/layers/terrain/tiles.ts
@@ -22,10 +22,19 @@ export const createTiles: (gl: WebGL2RenderingContext) => Tiles = (gl) => {
     gl,
     urlPattern: imageryUrl,
     onLoad: () => {
-      gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
-      gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.LINEAR);
+      gl.texParameteri(
+        gl.TEXTURE_2D,
+        gl.TEXTURE_MIN_FILTER,
+        gl.LINEAR_MIPMAP_LINEAR
+      );
+      gl.texParameteri(
+        gl.TEXTURE_2D,
+        gl.TEXTURE_MAG_FILTER,
+        gl.LINEAR_MIPMAP_LINEAR
+      );
       gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
       gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
+      gl.generateMipmap(gl.TEXTURE_2D);
     },
   });
 
