5acc9870de39c9e98874e9c6f6ccd20a8cc65a00
Graham <graham.gibbons@gmail.com>
2024-02-09 10:31:02 -0800

    Library build

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,18 @@
 import { defineConfig } from "vite";
 import glsl from "vite-plugin-glsl";
+import dts from "vite-plugin-dts";
 
 export default defineConfig({
   root: "src",
-  plugins: [glsl()],
+  plugins: [glsl(), dts()],
   build: {
     outDir: "../dist",
     sourcemap: true,
     target: "esnext",
+    lib: {
+      fileName: "index",
+      entry: "./index.ts",
+      formats: ["es"],
+    },
   },
 });
