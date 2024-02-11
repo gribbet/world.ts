1a24130e816be166d24d895c55111a8079cb6fa0
Graham <graham.gibbons@gmail.com>
2024-02-10 22:21:02 -0800

    Fix multiple terrain load

--- src/image-load.ts
+++ src/image-load.ts
@@ -1,7 +1,5 @@
 import createImageLoadWorker from "./image-load-worker?worker&inline";
 
-const worker = createImageLoadWorker();
-
 export type ImageLoad = {
   loaded: boolean;
   cancel: () => void;
@@ -15,6 +13,8 @@ export const createImageLoad = ({
   onLoad: (image: ImageBitmap | undefined) => void;
 }) => {
   let loaded = false;
+
+  const worker = createImageLoadWorker();
 
   const handler = ({ data }: MessageEvent) => {
     if (canceled || url !== data.url) return;
