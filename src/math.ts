4699ad6a13419c6018b641cd1eeecdddf48891d6
Graham <graham.gibbons@gmail.com>
2024-02-21 16:30:49 -0800

    Fix orientation

--- src/math.ts
+++ src/math.ts
@@ -39,13 +39,13 @@ export const tileToMercator = (
   return vec3.set(out, x * k, y * k, 0);
 };
 
-export const toQuaternion = ([pitch = 0, yaw = 0, roll = 0]: vec3) => {
-  const cy = Math.cos(yaw * 0.5);
-  const sy = Math.sin(yaw * 0.5);
+export const toQuaternion = ([pitch = 0, roll = 0, yaw = 0]: vec3) => {
+  const cy = Math.cos(roll * 0.5);
+  const sy = Math.sin(roll * 0.5);
   const cp = Math.cos(pitch * 0.5);
   const sp = Math.sin(pitch * 0.5);
-  const cr = Math.cos(roll * 0.5);
-  const sr = Math.sin(roll * 0.5);
+  const cr = Math.cos(yaw * 0.5);
+  const sr = Math.sin(yaw * 0.5);
 
   const w = cr * cp * cy + sr * sp * sy;
   const x = sr * cp * cy - cr * sp * sy;
@@ -56,8 +56,8 @@ export const toQuaternion = ([pitch = 0, yaw = 0, roll = 0]: vec3) => {
 };
 
 export const toOrientation = ([x = 0, y = 0, z = 0, w = 0]: quat) => {
-  const roll = Math.atan2(2 * (w * x + y * z), 1 - 2 * (x * x + y * y));
-  const pitch = Math.asin(2 * (w * y - z * x));
-  const yaw = Math.atan2(2 * (w * z + x * y), 1 - 2 * (y * y + z * z));
-  return [pitch, yaw, roll] satisfies vec3;
+  const yaw = Math.atan2(2 * (w * x + y * z), 1 - 2 * (x * x + y * y));
+  const pitch = Math.asin(Math.min(1, 2 * (w * y - z * x)));
+  const roll = Math.atan2(2 * (w * z + x * y), 1 - 2 * (y * y + z * z));
+  return [pitch, roll, yaw] satisfies vec3;
 };
