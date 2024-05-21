b6ae8da73d5112c17f8af2c2413e984b1db47538
Graham <graham.gibbons@gmail.com>
2024-05-20 21:06:41 -0700

    Adjust radar shader

--- src/layers/radar/fragment.glsl
+++ src/layers/radar/fragment.glsl
@@ -10,6 +10,7 @@ void main() {
    float d = length(uv_out.xy);
    if (d >= 1.0) discard;
    float a = atan(uv_out.y, uv_out.x) / 2. / 3.14159;
-   result = texture(image, vec2(d, a));
-   if (result.a == 0.f) discard;
+   vec4 value = texture(image, vec2(d, a));
+   if (value.r == 0.f) discard;
+   result = vec4(0, 1., 0, value.r);
 }
