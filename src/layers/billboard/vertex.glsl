4de76ca9b3b7fae5f601d7ee2098d057aa598d38
Graham <graham.gibbons@gmail.com>
2026-02-06 10:50:10 -0800

    Text layer support (#19)

--- src/layers/billboard/vertex.glsl
+++ src/layers/billboard/vertex.glsl
@@ -10,9 +10,11 @@ uniform vec2 image_size;
 uniform ivec3 position;
 uniform vec4 color;
 uniform float size;
+uniform vec2 offset;
+uniform float min_scale;
+uniform float max_scale;
 uniform float min_size_pixels;
 uniform float max_size_pixels;
-
 
 in vec2 corner;
 in vec2 uv;
@@ -21,15 +23,15 @@ out vec4 color_out;
 
 const int ONE = 1073741824; // 2^30
 const float INV_ONE = 1.f / float(ONE);
-const float CIRCUMFERENCE = 40075017.;
+const float CIRCUMFERENCE = 40075017.f;
 
 void main(void) {
     vec4 projected = projection * model_view * vec4(vec3(position - camera) * INV_ONE, 1.f);
 
     float pixel_size = projected.w / screen.y;
-    float scale = clamp(size / CIRCUMFERENCE * -projection[1][1], min_size_pixels * pixel_size, max_size_pixels * pixel_size);
+    float scale = clamp(size / CIRCUMFERENCE * -projection[1][1], max(min_scale * image_size.y, min_size_pixels) * pixel_size, min(max_scale * image_size.y, max_size_pixels) * pixel_size);
 
-    gl_Position = projected + scale * screen.y / image_size.y * vec4(corner * image_size / screen, 0.f, 0.0f);
+    gl_Position = projected + scale * screen.y / image_size.y * vec4((corner * image_size + 2.f * offset) / screen, 0.f, 0.0f);
 
     uv_out = uv;
     color_out = color;
